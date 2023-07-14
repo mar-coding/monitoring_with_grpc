a901cef92eeed919ad2e9afa93c7aa10aa17ae87
Amin Rahimi <60325191+mar-coding@users.noreply.github.com>
2023-07-14 12:29:47 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Creating proto grpc files..."
 python -m grpc_tools.protoc -I=proto/ --python_out=proto/ --grpc_python_out=proto/ proto/chat.proto
+sed -i 's/import chat_pb2 as chat__pb2/import proto.chat_pb2 as chat__pb2/g' proto/chat_pb2_grpc.py
 mkdir ./logs
 echo "DONE"
